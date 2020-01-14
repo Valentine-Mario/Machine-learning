d48e58b6882bd4f260b9f279d6ba661d3e012568
valentine-mario <valentine13400@gmail.com>
2020-01-14 11:14:30 +0100

    completed question on  exploring enron dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,8 +16,8 @@
 """
 
 import pickle
-enron_data = pickle.load(open("./final_project/final_project_dataset.pkl", "rb"))
-people_of_interest= open("./final_project/poi_names.txt", "r")
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+people_of_interest= open("../final_project/poi_names.txt", "r")
 #number of individuals in list
 print len(enron_data)
 
@@ -36,7 +36,7 @@ for a in enron_data:
 print num_of_POI
 
 
-num_lines = sum(1 for line in open("./final_project/poi_names.txt", "r"))
+num_lines = sum(1 for line in open("../final_project/poi_names.txt", "r"))
 print people_of_interest.read()
 print num_lines
 
@@ -51,3 +51,37 @@ print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
 #number of stock options from SKILLING K JEFFREY
 # print enron_data.keys()    
 print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+print "#######################################"
+print enron_data['SKILLING JEFFREY K']['total_payments']
+print enron_data['LAY KENNETH L']['total_payments']
+print enron_data['FASTOW ANDREW S']['total_payments']
+print "####################"
+
+
+qty_salary=0
+for i in enron_data.values():
+    if i['salary']!='NaN':
+        qty_salary+=1
+
+#peole with quantifiable salary
+print qty_salary
+
+known_email=0
+for i in enron_data.values():
+   if i['email_address']!='NaN':
+       known_email+=1
+
+#people with known email
+print known_email
+
+print "#############################"
+#number of people with nan as total payment
+no_payment=list(filter(lambda x: (x['total_payments']=='NaN') , enron_data.values()))
+print len(no_payment)
+print "#####################"
+no_POI=list(filter(lambda x: (x['poi']) , enron_data.values()))
+POI_no_payment=list(filter(lambda x: (x['total_payments']=='NaN'), no_POI))
+print len(POI_no_payment)
+
+print len(list(filter(lambda x: (x['poi']) , enron_data.values())))
