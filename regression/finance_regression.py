ce2175ac878eb4a959ac1bd56dda4b8fa5a7651c
valentine-mario <valentine13400@gmail.com>
2020-01-15 17:16:17 +0100

    finished lessons and questions on regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -18,6 +18,8 @@ import pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
+from sklearn.linear_model import LinearRegression
+
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
@@ -26,10 +28,10 @@ data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,10 +40,22 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = LinearRegression().fit(feature_train, target_train)
+
+#print out slope
+print reg.coef_
+
+#print out intercept
+print reg.intercept_
+
+#score on the training data
+print reg.score(feature_train, target_train)
+
+#score on testing data
+print reg.score(feature_test, target_test)
 
 
-
-
+   # print i, dictionary[i]['long_term_incentive']
 
 
 
@@ -64,6 +78,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+lin=LinearRegression().fit(feature_test, target_test)
+plt.plot(feature_train, lin.predict(feature_train), color="b")
+print lin.coef_
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
