f2760532d4938f5c81e1ff141fa36cf6495f19e0
valentine-mario <valentine13400@gmail.com>
2020-01-29 14:51:58 +0100

    finished lesson on feature selection

---
+++ feature_selection.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#importing libraries
+from sklearn.datasets import load_boston
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+'exec(%matplotlib inline)'
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
+
+x = load_boston()
+df = pd.DataFrame(x.data, columns = x.feature_names)
+df["MEDV"] = x.target
+X = df.drop("MEDV",1)   #Feature Matrix
+y = df["MEDV"]          #Target Variable
+df.head()
+
+#Using Pearson Correlation
+plt.figure(figsize=(12,10))
+#Compute pairwise correlation of columns, excluding NA/null values
+cor = df.corr()
+sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
+plt.show()
+
+#Correlation with output variable
+cor_target = abs(cor["MEDV"])
+#Selecting highly correlated features
+relevant_features = cor_target[cor_target>0.5]
+print(relevant_features)
+
+#check the correlation of features with each other
+print(df[["LSTAT","PTRATIO"]].corr())
+#rm and lsat seem to be highly correlated to each other
+print(df[["RM","LSTAT"]].corr())
+
+#embedded method using lasso
+reg = LassoCV()
+reg.fit(X, y)
+print("Best alpha using built-in LassoCV: %f" % reg.alpha_)
+print("Best score using built-in LassoCV: %f" %reg.score(X,y))
+coef = pd.Series(reg.coef_, index = X.columns)
+print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
+#relevant features picked by lasso
+relevant_feat2=coef[coef!=0]
+print(relevant_feat2)
+
+# imp_coef = coef.sort_values()
+# import matplotlib
+# matplotlib.rcParams['figure.figsize'] = (8.0, 10.0)
+# imp_coef.plot(kind = "barh")
+# plt.title("Feature importance using Lasso Model")
--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -1,3 +1,5 @@
+
+
 #!/usr/bin/python
 
 import pickle
@@ -8,8 +10,8 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "/home/dead/Documents/python/ud120-projects/your_word_data.pkl" 
+authors_file = "/home/dead/Documents/python/ud120-projects/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -19,8 +21,8 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -38,6 +40,18 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print "accuracy:", clf.score(features_test, labels_test)
+feat_impo=[]
 
+for i in range(len(clf.feature_importances_)):
+    if(clf.feature_importances_[i]>0.2):
+        feat_impo.append(i)
+        print clf.feature_importances_[i]
 
+print len(feat_impo)
 
+for i in feat_impo:
+   print vectorizer.get_feature_names()[i]
